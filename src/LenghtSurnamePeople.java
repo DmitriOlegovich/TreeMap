3e9544270e35bfa5b6963de8fbe098239f441b3b
Dmitriy Prosolov <prosolovdmitri@mail.ru>
2023-03-29 10:41:57 +0300

    Сщхраняем изменения сделанные для задачи 2

---
+++ src/LenghtSurnamePeople.java
@@ -0,0 +1,36 @@
+import java.util.Comparator;
+
+public class LenghtSurnamePeople implements Comparator<Person> {
+    protected int maxSurname;
+    protected int firstLength;
+    protected int secondLength;
+
+    public void LenghtSurnamePeople(int maxSurname) {
+        this.maxSurname = maxSurname;
+    }
+
+    @Override
+    public int compare(Person o1, Person o2) {
+        String[] people1 = o1.getSurname().split(" ");
+        String[] people2 = o2.getSurname().split(" ");
+        firstLength = people1.length;
+        secondLength = people2.length;
+
+        if (people1.length > maxSurname)
+            firstLength = maxSurname;
+
+
+        if (people2.length > maxSurname) {
+            secondLength = maxSurname;
+
+
+            if (firstLength == secondLength) {
+                return o1.getAge() - o2.getAge();
+            } else {
+                return firstLength - secondLength;
+            }
+        }
+        return 0;
+    }
+}
+
--- src/Person.java
+++ src/Person.java
@@ -21,4 +21,5 @@ public class Person{
     public String toString() {
         return name + " " + surname + " " + age;
     }
+
 }
